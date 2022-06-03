5c87697b9f77f1b67023c5798b38ed40b08251f9
George Iulian Diaconu <kuhlin@gmail.com>
2022-06-03 13:00:38 +0200

    Change the port

--- index.js
+++ index.js
@@ -77,5 +77,5 @@ function validateBook(book) {
 }
 
 //PORT ENVIRONMENT VARIABLE
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening on port ${port}..'));
